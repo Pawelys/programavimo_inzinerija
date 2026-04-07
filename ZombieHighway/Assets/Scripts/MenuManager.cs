22a4852d3f3f7191236e8f2ba5e8c279597cf444
Pawelys <diisiisart@gmail.com>
2026-04-07 14:46:59 +0300

    Home screen button functionality
    + Play buttons starts the game
    + Settings button opens Settings_Scene(Empty)
    +Quit button should quit the application

--- ZombieHighway/Assets/Scripts/MenuManager.cs
+++ ZombieHighway/Assets/Scripts/MenuManager.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MenuManager
+public class MenuManager : MonoBehaviour
 {
-    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Scene"); // or use index: LoadScene(1)
+    }
+
+    public void OpenSettings()
+    {
+        // Load settings panel or scene
+        SceneManager.LoadScene("Settings_Scene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Quit"); // visible in editor
+    }
 }
