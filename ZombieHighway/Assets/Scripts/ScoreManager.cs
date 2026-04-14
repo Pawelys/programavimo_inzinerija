e378bd92e0f77ce70e6516700c8a0f7b6e218828
janq005 <jankunas980@gmail.com>
2026-04-14 16:48:45 +0300

    SCRUM-27: Auto-create score UI at runtime

--- ZombieHighway/Assets/Scripts/ScoreManager.cs
+++ ZombieHighway/Assets/Scripts/ScoreManager.cs
@@ -4,10 +4,7 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance;
-
-    [Header("UI")]
-    public Text scoreText;
-
+    private Text _scoreText;
     private int _score = 0;
 
     void Awake()
@@ -15,7 +12,41 @@ public class ScoreManager : MonoBehaviour
         if (Instance == null)
             Instance = this;
         else
+        {
             Destroy(gameObject);
+            return;
+        }
+
+        CreateUI();
+    }
+
+    void CreateUI()
+    {
+        GameObject canvasObj = new GameObject("ScoreCanvas");
+        Canvas canvas = canvasObj.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        canvasObj.AddComponent<CanvasScaler>();
+        canvasObj.AddComponent<GraphicRaycaster>();
+
+        GameObject textObj = new GameObject("ScoreText");
+        textObj.transform.SetParent(canvasObj.transform);
+        _scoreText = textObj.AddComponent<Text>();
+        _scoreText.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
+        _scoreText.fontSize = 32;
+        _scoreText.color = Color.white;
+        _scoreText.alignment = TextAnchor.UpperLeft;
+        _scoreText.text = "Score: 0";
+
+        RectTransform rt = _scoreText.GetComponent<RectTransform>();
+        rt.anchorMin = new Vector2(0, 1);
+        rt.anchorMax = new Vector2(0, 1);
+        rt.pivot = new Vector2(0, 1);
+        rt.anchoredPosition = new Vector2(20, -20);
+        rt.sizeDelta = new Vector2(300, 50);
+
+        Outline outline = textObj.AddComponent<Outline>();
+        outline.effectColor = Color.black;
+        outline.effectDistance = new Vector2(2, -2);
     }
 
     void Start()
@@ -36,7 +67,7 @@ public class ScoreManager : MonoBehaviour
 
     private void UpdateUI()
     {
-        if (scoreText != null)
-            scoreText.text = "Score: " + _score;
+        if (_scoreText != null)
+            _scoreText.text = "Score: " + _score;
     }
 }
